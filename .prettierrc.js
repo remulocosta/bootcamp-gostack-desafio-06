fce17b3571537c31f3bc1826209fdf3e83491ba1
Remulo Costa <remulo.costa@gmail.com>
2019-12-06 23:19:52 -0300

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+{
+ // bracketSpacing: true,
+  // jsxBracketSameLine: false,
+ // "singleQuote": true,
+ // "trailingComma": 'es5',
+}
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'es5',
+};
